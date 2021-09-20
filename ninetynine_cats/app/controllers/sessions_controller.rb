bbaf36fce1940028d4c9a0b5cb1363abdd22f6ed
mrossant <matteorossant@gmail.com>
2021-09-20 16:02:11 -0400

    Brushing up Sessions

--- ninetynine_cats/app/controllers/sessions_controller.rb
+++ ninetynine_cats/app/controllers/sessions_controller.rb
@@ -1,1 +1,27 @@
+class SessionsController < ApplicationController
+    def new
+        @user = User.new
+        # render :new
+    end
 
+    def create
+        @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
+
+        if @user
+            # new_session_token = @user.reset_session_token!
+            # session[:session_token] = new_session_token
+            login(@user)
+            redirect_to cats_url
+        else
+            redirect_to cats_url
+            #render :new
+        end
+    end
+
+    def destroy
+        @current_user.reset_session_token! if logged_in?
+
+        session[:session_token] = nil
+        @current_user = nil
+    end
+end
